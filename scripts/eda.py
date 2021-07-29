a28e653dcf3e6da3c56af3c7aa64673509d39ebc
Aitor <aitirga@gmail.com>
2021-07-30 01:55:43 +0200

    feat: add capacity to generate animated gif videos for each patient

--- scripts/eda.py
+++ scripts/eda.py
@@ -2,12 +2,18 @@
 This script contains various useful methods to carry out Exploratory Data Analysis (EDA)
 The dicom reading method has been taken from 'https://www.kaggle.com/raddar/convert-dicom-to-np-array-the-correct-way'
 '''
+import pathlib
+
 import numpy as np
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
-
+from pathlib import Path
 import matplotlib.pyplot as plt
+import imageio
+import natsort
+import logging
 
+logger = logging.getLogger(__file__)
 
 def read_mri(path, voi_lut=True, fix_monochrome=True):
     dicom = pydicom.read_file(path)
@@ -26,3 +32,34 @@ def read_mri(path, voi_lut=True, fix_monochrome=True):
     data = data / np.max(data)
     data = (data * 255).astype(np.uint8)
     return data
+
+
+def find_images_for_case(path) -> dict:
+    """This method finds and orders the images corresponding to a given case
+    Args:
+        path: path of the case
+
+    Returns:
+        A dictionary containing the image paths
+    """
+    case_path = Path(path)
+    case_dict = {}
+    case_dict['flair'] = natsort.natsorted(list(Path(case_path / "FLAIR").glob('*.dcm')),
+                                           key=lambda x: str(x))
+    case_dict['t1w'] = natsort.natsorted(list(Path(case_path / "T1w").glob('*.dcm')),
+                                         key=lambda x: str(x))
+    case_dict['t1wce'] = natsort.natsorted(list(Path(case_path / "T1wCE").glob('*.dcm')), key=lambda x: str(x))
+    case_dict['t2w'] = natsort.natsorted(list(Path(case_path / "T2w").glob('*.dcm')), key=lambda x: str(x))
+
+    return case_dict
+
+
+def make_gif(path: pathlib.Path, set='flair'):
+    case_dict = find_images_for_case(path)
+    images = []
+    for filename in case_dict[set]:
+        images.append(read_mri(filename))
+    output_name = f"{path.stem}-{set}.gif"
+    imageio.mimsave(output_name, images)
+
+
---
+++ scripts/test_eda/test_get_image_dict.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from config import config
+import logging
+from scripts.eda import read_mri, find_images_for_case, make_gif
+
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logger.info('Read a sample data')
+    data_file = Path(config.paths.data_folder) / "train/00000/"
+    # image_dict = find_images_for_case(data_file)
+    make_gif(data_file)
+
+
+
